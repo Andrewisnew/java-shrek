22c3f1055c409a6b256a4cd6e7907a151460d283
Данил <Kremevdd97@yandex.ru>
2020-11-10 00:12:05 +0300

    Shrek-Privet

--- src/main/java/com/shrek/task00001/Main.java
+++ src/main/java/com/shrek/task00001/Main.java
@@ -1,6 +1,5 @@
 package com.shrek.task00001;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Группа людей участвует в марафоне, их имена и время за которое они пробежали марафон представленны в массиве.
@@ -9,10 +8,73 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class Main {
     public String getWinner(String[] runners, int[] times) {
-        throw new NotImplementedException();
+       int[] shrek =  new int[times.length];
+              shrek = times;
+       int winnerTime;
+
+       boolean sort=true;
+       String winnerName;
+       while (sort) {
+           sort=false;
+           for (int i = 1; i <shrek.length-1; i++) {
+               if (shrek[i] < shrek[i - 1]) {
+                   int change = shrek[i];
+                   shrek[i] = shrek[i - 1];
+                   shrek[i - 1] = change;
+                   sort = true;
+               }
+
+           }
+       }
+       int winnerPos = 0;
+
+
+       for (int pos=0;pos<=times.length-1;pos++)
+            {
+              if (times[pos]==shrek[0])
+                {
+                    winnerPos=pos;
+                }
+              else
+                  break;
+
+            }
+
+        winnerName=runners[winnerPos];
+
+        return winnerName;
     }
 
     public String getSecond(String[] runners, int[] times) {
-        throw new NotImplementedException();
+        int[] kersh=times;
+        int secondTime;
+        int secondPos=0;
+        String secondName;
+        boolean sort = true;
+        while (sort) {
+            sort = false;
+            for (int i = 0; i < kersh.length - 1; i++) {
+                if (kersh[i] < kersh[i + 1])
+                {
+                    int change = kersh[i];
+                    kersh[i] = kersh[i + 1];
+                    kersh[i + 1] = change;
+                    sort = true;
+                }
+
+            }
+        }
+        secondTime = kersh[1];
+            for (int pos=0;pos<times.length-1;pos++)
+            {
+                if (times[pos]==secondTime)
+                {
+                    secondPos=pos;
+                }
+            }
+
+        secondName=runners[secondPos];
+
+        return secondName;
     }
 }
--- src/test/java/com/shrek/task00001/MainTest.java
+++ src/test/java/com/shrek/task00001/MainTest.java
@@ -16,8 +16,8 @@ public class MainTest {
 
     @Test
     public void test1() {
-        String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
-        int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
+        String[] names = {"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"};
+        int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
         String winner = main.getWinner(names, times);
         assertEquals("John", winner);
         String second = main.getSecond(names, times);
@@ -26,8 +26,8 @@ public class MainTest {
 
     @Test
     public void test2() {
-        String[] names = { "Elena" };
-        int[] times = { 341 };
+        String[] names = {"Elena"};
+        int[] times = {341};
         String winner = main.getWinner(names, times);
         assertEquals("Elena", winner);
         String second = main.getSecond(names, times);
@@ -36,8 +36,8 @@ public class MainTest {
 
     @Test
     public void test3() {
-        String[] names = { "Elena", "Thomas" };
-        int[] times = { 341, 215 };
+        String[] names = {"Elena", "Thomas"};
+        int[] times = {341, 215};
         String winner = main.getWinner(names, times);
         assertEquals("Thomas", winner);
         String second = main.getSecond(names, times);
@@ -46,8 +46,8 @@ public class MainTest {
 
     @Test
     public void test4() {
-        String[] names = { "Elena", "Thomas" };
-        int[] times = { 141, 215 };
+        String[] names = {"Elena", "Thomas"};
+        int[] times = {141, 215};
         String winner = main.getWinner(names, times);
         assertEquals("Elena", winner);
         String second = main.getSecond(names, times);
@@ -56,8 +56,8 @@ public class MainTest {
 
     @Test
     public void test5() {
-        String[] names = {  };
-        int[] times = {  };
+        String[] names = {};
+        int[] times = {};
         String winner = main.getWinner(names, times);
         assertNull(winner);
         String second = main.getSecond(names, times);
