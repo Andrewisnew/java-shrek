1763949ea74683d13e67afe597cccc182a267e5b
Данил <Kremevdd97@yandex.ru>
2020-11-13 21:41:38 +0300

    Sorting removed.Check it.

--- src/main/java/com/shrek/task00001/Main.java
+++ src/main/java/com/shrek/task00001/Main.java
@@ -8,66 +8,58 @@ package com.shrek.task00001;
  */
 public class Main {
     public String getWinner(String[] runners, int[] times) {
-        int[] shrek = new int[times.length];
-        System.arraycopy(times, 0, shrek, 0, times.length);
-        //int winnerTime;
-        boolean sort = true;
+
         String winnerName;
         if (times.length == 0) {
             return null;
         }
-        while (sort) {
-            sort = false;
-            for (int i = 1; i <= shrek.length - 1; i++) {
-                if (shrek[i] < shrek[i - 1]) {
-                    int change = shrek[i];
-                    shrek[i] = shrek[i - 1];
-                    shrek[i - 1] = change;
-                    sort = true;
-                }
+        int min1 = times[0];
 
+        for (int i = 0; i <= times.length - 1; i++) {
+            if (times[i] < min1) {
+                min1 = times[i];
             }
         }
         int winnerPos = 0;
-
-
         for (int pos = 0; pos <= times.length - 1; pos++) {
-            if (times[pos] == shrek[0]) {
+            if (times[pos] == min1) {
                 winnerPos = pos;
             }
-
         }
-
         winnerName = runners[winnerPos];
-
         return winnerName;
     }
 
     public String getSecond(String[] runners, int[] times) {
-        int[] kersh = new int[times.length];
-        System.arraycopy(times, 0, kersh, 0, times.length);
-        int secondTime;
-        int secondPos = 0;
-        String secondName;
-        boolean sort = true;
         if (times.length < 2) {
             return null;
         }
-        while (sort) {
-            sort = false;
-            for (int i = 0; i < kersh.length - 1; i++) {
-                if (kersh[i] < kersh[i + 1]) {
-                    int change = kersh[i];
-                    kersh[i] = kersh[i + 1];
-                    kersh[i + 1] = change;
-                    sort = true;
-                }
+        int min1 = times[0];
+        int min2 = times[1];
+        int min3 = times[times.length - 1];
+        int min4 = times[times.length - 2];
 
+        int secondTime;
+        int secondPos = 0;
+        String secondName;
+        for (int i = 0; i <= times.length - 1; i++) {
+            if (min1 > times[i]) {
+                min2 = min1;
+                min1 = times[i];
+            }
+        }
+        for (int p = times.length - 1; p >= 0; p--) {
+            if (min3 > times[p]) {
+                min4 = min3;
+                min3 = times[p];
             }
         }
 
+        if (min2 < min4) {
+            secondTime = min2;
+        } else
+            secondTime = min4;
 
-        secondTime = kersh[times.length - 2];
         for (int pos = 0; pos < times.length; pos++) {
             if (times[pos] == secondTime) {
                 secondPos = pos;
