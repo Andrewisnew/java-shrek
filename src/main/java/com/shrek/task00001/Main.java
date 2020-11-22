2e33c2805cb9980064291e556c555297ce99564e
Данил <Kremevdd97@yandex.ru>
2020-11-23 00:00:46 +0300

    Sorting removed.Check it.

--- src/main/java/com/shrek/task00001/Main.java
+++ src/main/java/com/shrek/task00001/Main.java
@@ -4,61 +4,52 @@ package com.shrek.task00001;
 /**
  * Группа людей участвует в марафоне, их имена и время за которое они пробежали марафон представленны в массиве.
  * Необходимо найти человека, который быстрее всех пробежал дистанцию и вывести его имя.
- * Найти человека, который прибежал вторым.
+ * Найти  человека, который прибежал вторым.
  */
 public class Main {
     public String getWinner(String[] runners, int[] times) {
 
-        String winnerName;
         if (times.length == 0) {
             return null;
         }
         int min1 = times[0];
-
+        int winnerPos = 0;
         for (int i = 0; i <= times.length - 1; i++) {
             if (times[i] < min1) {
                 min1 = times[i];
             }
-        }
-        int winnerPos = 0;
-        for (int pos = 0; pos <= times.length - 1; pos++) {
-            if (times[pos] == min1) {
-                winnerPos = pos;
+            if (times[i] == min1) {
+                winnerPos = i;
             }
         }
-        winnerName = runners[winnerPos];
-        return winnerName;
+        return runners[winnerPos];
     }
 
     public String getSecond(String[] runners, int[] times) {
         if (times.length < 2) {
             return null;
         }
-        int min1 = times[0];
-        int min2 = times[1];
-        int min3 = times[times.length - 1];
-        int min4 = times[times.length - 2];
+        int winnerTime = times[0];
+        int secondTime = times[1];
 
-        int secondTime;
         int secondPos = 0;
         String secondName;
-        for (int i = 0; i <= times.length - 1; i++) {
-            if (min1 > times[i]) {
-                min2 = min1;
-                min1 = times[i];
-            }
-        }
-        for (int p = times.length - 1; p >= 0; p--) {
-            if (min3 > times[p]) {
-                min4 = min3;
-                min3 = times[p];
+
+        if (times.length == 2) {
+            if (times[0] > times[1]) {
+                winnerTime = times[1];
+                secondTime = times[0];
             }
         }
 
-        if (min2 < min4) {
-            secondTime = min2;
-        } else
-            secondTime = min4;
+        for (int i = 2; i <= times.length - 1; i++) {
+            if (winnerTime > times[i] && secondTime > times[i]) {
+                winnerTime = times[i];
+            }
+            if (winnerTime < times[i] && secondTime > times[i]) {
+                secondTime = times[i];
+            }
+        }
 
         for (int pos = 0; pos < times.length; pos++) {
             if (times[pos] == secondTime) {
@@ -67,7 +58,7 @@ public class Main {
         }
 
         secondName = runners[secondPos];
-
         return secondName;
     }
 }
+ 
