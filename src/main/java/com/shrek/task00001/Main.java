6c18ffefe1e9af9607c37cdc968468edee38f818
Данил <Kremevdd97@yandex.ru>
2020-12-06 22:20:53 +0300

    1

--- src/main/java/com/shrek/task00001/Main.java
+++ src/main/java/com/shrek/task00001/Main.java
@@ -12,11 +12,9 @@ public class Main {
         if (times.length == 0) {
             return null;
         }
-        int winnerTime = times[0];
         int winnerPos = 0;
         for (int i = 0; i <= times.length - 1; i++) {
-            if (times[i] <= winnerTime) {
-                winnerTime = times[i];
+            if (times[i] <= times[winnerPos]) {
                 winnerPos = i;
             }
         }
@@ -27,30 +25,24 @@ public class Main {
         if (times.length < 2) {
             return null;
         }
-        int secondTime = times[1];
         int secondPos = 1;
         int winnerPos = 0;
-        String secondName;
         if (times[0] > times[1]) {
-            secondTime = times[0];
             secondPos = 0;
             winnerPos = 1;
         }
         for (int i = 2; i <= times.length - 1; i++) {
-            if (times[i] < secondTime && times[i] > times[winnerPos]) {
-                secondTime = times[i];
+            if (times[i] < times[secondPos] && times[i] > times[winnerPos]) {
                 secondPos = i;
                 continue;
             }
             if (times[i] <= times[winnerPos]) {
-                secondTime = times[winnerPos];
                 secondPos = winnerPos;
                 winnerPos = i;
                 continue;
             }
         }
-        secondName = runners[secondPos];
-        return secondName;
+        return runners[secondPos];
     }
 }
  
