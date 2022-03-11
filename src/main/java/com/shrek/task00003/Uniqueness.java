e12a9e1b9847afb91059984d2cd248269ab209d7
Andrey Zakomornyy <zakomornii1998@gmail.com>
2022-03-11 13:40:36 +0300

    add task00003

---
+++ src/main/java/com/shrek/task00003/Uniqueness.java
@@ -0,0 +1,18 @@
+package com.shrek.task00003;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * Класс в конструкторе принимает список строк. Необхдимо реализовать 3 метода:
+ * 1. Есть ли в списке дубликаты
+ * 2. Сколько дублирующихся элементов
+ * 3. Сколько каждый элемент раз встречается
+ *
+ * Написать тесты
+ */
+public class Uniqueness {
+    public Uniqueness(@Nonnull List<String> elements) {
+
+    }
+}
